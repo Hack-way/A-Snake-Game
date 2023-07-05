5bd8e1889d95c504148a074be07fdb444b4ddfff
Hackway <136243694+Hack-way@users.noreply.github.com>
2023-07-05 12:21:41 -0700

    Add files via upload

---
+++ Snake Game with @Hackway/main.js
@@ -0,0 +1,195 @@
+// Set up the canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+
+// Define the obstacles
+const obstacles = [
+  { x: 5, y: 5 },
+  { x: 20, y: 20 },
+  { x: 20, y: 15 },
+  { x: 10, y: 20 }
+];
+
+// Set up the game variables
+let snake = [{ x: 10, y: 10 }];
+let food = { x: 0, y: 0 };
+let direction = "right";
+let score = 0;
+let speed = 110; // Default speed (easy level)
+
+// Set up the game loop
+function gameLoop() {
+  // Move the snake
+  let head = { x: snake[0].x, y: snake[0].y };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "right":
+      head.x++;
+      break;
+  }
+  snake.unshift(head);
+
+  // Check for collision with food
+  if (head.x === food.x && head.y === food.y) {
+    score++;
+    generateFood();
+    speed -= 5; // Increase speed (medium level)
+  } else {
+    snake.pop();
+  }
+
+  // Check for collision with walls or self
+  if (head.x < 0) {
+    head.x = Math.floor(width / 10) - 1;
+  } else if (head.x >= Math.floor(width / 10)) {
+    head.x = 0;
+  }
+  if (head.y < 0) {
+    head.y = Math.floor(height / 10) - 1;
+  } else if (head.y >= Math.floor(height / 10)) {
+    head.y = 0;
+  }
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      gameOver();
+      return;
+    }
+  }
+  for (let i = 0; i < obstacles.length; i++) {
+    if (head.x === obstacles[i].x && head.y === obstacles[i].y) {
+      gameOver();
+      return;
+    }
+  }
+
+  // Draw the game
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillRect(snake[i].x * 10, snake[i].y * 10, 10, 10);
+  }
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x * 10, food.y * 10, 10, 10);
+  ctx.fillStyle = "black";
+  ctx.fillText(`Score: ${score}`, 10, 20);
+
+  ctx.fillStyle = "#9E9E9E"; // gray
+  for (let i = 0; i < obstacles.length; i++) {
+    ctx.fillRect(obstacles[i].x * 10, obstacles[i].y * 10, 10, 10);
+  }
+
+  // Call the game loop again
+  setTimeout(gameLoop, speed);
+}
+
+// Set up the start button functionality
+const startDiv = document.getElementById("start");
+const startButton = document.querySelector("#start button");
+
+function startGame() {
+  startDiv.style.display = "none";
+  canvas.style.display = "block";
+  generateFood();
+  gameLoop();
+}
+
+// Set up the game over function
+function gameOver() {
+  alert(`Game over! Your score is ${score}.`);
+  snake = [{ x: 10, y: 10 }];
+  food = { x: 0, y: 0 };
+  direction = "right";
+  score = 0;
+  speed = 100; // Reset speed to default (easy level)
+  generateFood();
+  startDiv.style.display = "block";
+  canvas.style.display = "none";
+}
+
+// Set up the food generation function
+function generateFood() {
+  let x, y;
+  do {
+    x = Math.floor(Math.random() * width / 10);
+    y = Math.floor(Math.random() * height / 10);
+  } while (snake.some(segment => segment.x === x && segment.y === y) || obstacles.some(obstacle => obstacle.x === x && obstacle.y === y));
+  
+  food = { x, y };
+}
+
+// Add touch controls
+let touchStartX = 0;
+let touchStartY = 0;
+document.addEventListener("touchstart", (event) => {
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+});
+document.addEventListener("touchmove", (event) => {
+  const touchEndX = event.touches[0].clientX;
+  const touchEndY = event.touches[0].clientY;
+  const dx = touchEndX - touchStartX;
+  const dy = touchEndY - touchStartY;
+  if (Math.abs(dx) > Math.abs(dy)) {
+    // Horizontal swipe
+    if (dx > 0) {
+      // Right swipe
+      if (direction !== "left") {
+        direction = "right";
+      }
+    } else {
+      // Left swipe
+      if (direction !== "right") {
+        direction = "left";
+      }
+    }
+  } else {
+    // Vertical swipe
+    if (dy > 0) {
+      // Down swipe
+      if (direction !== "up") {
+        direction = "down";
+      }
+    } else {
+      // Up swipe
+      if (direction !== "down") {
+        direction = "up";
+      }
+    }
+  }
+});
+
+// Set up the keyboard controls
+document.addEventListener("keydown", (event) => {
+  switch (event.keyCode) {
+    case 38: // up arrow
+      if (direction !== "down") {
+        direction = "up";
+      }
+      break;
+    case 40: // down arrow
+      if (direction !== "up") {
+        direction = "down";
+      }
+      break;
+    case 37: // left arrow
+      if (direction !== "right") {
+        direction = "left";
+      }
+      break;
+    case 39: // right arrow
+      if (direction !== "left") {
+        direction = "right";
+      }
+      break;
+  }
+});
